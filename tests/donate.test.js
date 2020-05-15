ce22b7d7efb79521f81f1d883b3a4221f3faf3a2
Daniel Fernandez <oo@OOs-MacBook-Pro.local>
2020-05-15 13:17:58 -0300

    add missing address element

--- tests/donate.test.js
+++ tests/donate.test.js
@@ -95,38 +95,13 @@ describe("Graph", function() {
     //Add element to graph
     let id9 = graph.connectElements([[id8, 0]], element, 1, 4, 0);
 
+    element = elements.ADDRESS;
+    result = graph.isElementReadyToCreate(element);
+    expect(result).to.be.true;
+    //Add element to graph
+    let id10 = graph.connectElements([[id8, 1]], element, 1, 4, 0);
+
     console.log(JSON.stringify(graph.elements));
 
-    // const op_uniswap_ETH_to_DAI = await factory.createOperationElement(
-    //   web3,
-    //   operations.OP_UNISWAP_ETH_TO_DAI,
-    //   "0x0"
-    // );
-    // graph.addElement(inputETH, 0, op_uniswap_ETH_to_DAI, 0);
-    // graph.addElement(op_uniswap_WBTC_to_ETH, 0, op_uniswap_ETH_to_DAI, 0);
-    // graph.addElement(op_uniswap_USDC_to_ETH, 0, op_uniswap_ETH_to_DAI, 0);
-
-    // const splitter = factory.createSplitterElement(web3, 0.5); //50% 50%
-    // graph.addElement(inputDAI, 0, splitter, 0);
-    // graph.addElement(op_uniswap_ETH_to_DAI, 0, splitter, 0);
-
-    // const address1 = "0x3B8B8830977A3664fF169769c8b67baade0e5f22";
-    // const address2 = "0x9E19819809927326BdB8C35198673F608c46E658";
-    // const addressElement1 = factory.createAddressElement(address1);
-    // const addressElement2 = factory.createAddressElement(address2);
-
-    // graph.addElement(splitter, 0, addressElement1, 0);
-    // graph.addElement(splitter, 1, addressElement2, 0);
-
-    // //Deploy graph
-    // const graphAddress = factory.deployGraph(web3, graph);
-
-    //Load graph
-    //const loadedGraph = await factory.loadGraph(graphAddress);
-
-    //Execute graph
-    //graph.setInputValue(0, "500000000000"); //Donate DAI
-    //graph.setInputValue(2, "3300000000000"); //Donate USDC
-    //await loadedGraph.execute();
   });
 });
