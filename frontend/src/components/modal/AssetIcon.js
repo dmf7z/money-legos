a60f139dc57b80565234a9eb6822c2629de31033
brunitob <brunitob@gmail.com>
2020-05-18 02:13:27 -0300

    Update AssetIcon.js

--- frontend/src/components/modal/AssetIcon.js
+++ frontend/src/components/modal/AssetIcon.js
@@ -3,10 +3,7 @@ import { ASSETS_COLORS } from "../../constants";
 
 export const AssetIcon = (props) => {
   switch (props.asset) {
-    case 'ETH':
-      return <img className="modal__asset-icon" src={require('../../assets/icons/eth.svg')} />
     default:
       return <img className="modal__asset-icon" src={require(`../../assets/icons/${props.asset}.svg`)}></img>
-
   }
 };
